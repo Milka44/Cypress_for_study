f4963907db124df289589e9de033da24074224aa
Milka44 <po4tafor4@gmail.com>
2024-07-10 17:58:06 +0300

    some new tests are added

--- cypress/support/steps/WellcomePageSteps.js
+++ cypress/support/steps/WellcomePageSteps.js
@@ -3,19 +3,22 @@ import wellcomePage from "../pages/WellcomePage";
 const wellcomePageSteps = {
 	clickEveryHeaderButton() {
 		wellcomePage.getLogoBtn().click();
-		wellcomePage.getAboutBtn().click();
-		wellcomePage.getContactsBtn().click();
-		wellcomePage.getGuestLoginBtn().click();
+		wellcomePage.getHomeBtn().contains('Home').click();
+
+		wellcomePage.getAboutBtn().contains('About').click();
+		wellcomePage.getContactsBtn().contains('Contacts').click();
+		wellcomePage.getGuestLoginBtn().contains('Guest log in').click();
 		wellcomePage.getLogout().click();
-		wellcomePage.getSignInBtn().click();
+		wellcomePage.getSignInBtn().contains('Sign In').click();
 		wellcomePage.closeModal().click();
 		wellcomePage.getFbBtn().click();
 		wellcomePage.getTgBtn().click();
 		wellcomePage.geticonYoutobeBtn().click();
 		wellcomePage.geticonInstagramBtn().click();
-		wellcomePage.getContactURLLink().click();
+		wellcomePage.getContactURLLink().should('have.css', 'Color','rgb(2, 117, 216)')
+		wellcomePage.getContactURLLink().contains('ithillel.ua').click();
 		wellcomePage.getContactsEmailLink().contains('support@ithillel.ua')
-		wellcomePage.getContactsEmailLink().click()
+		//wellcomePage.getContactsEmailLink().click()
 }}
 
 export default wellcomePageSteps;
